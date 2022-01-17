daff8e486b92e8870a110f13d4bc482f38808c2b
YouJinTou <dandreevd@gmail.com>
2022-01-17 19:15:33 +0200

    Added interfaces.

--- src/services/google-sheets.ts
+++ src/services/google-sheets.ts
@@ -4,6 +4,14 @@ export interface SheetsProps {
     docId: string | undefined,
 };
 
+export interface Data {
+    rows: any[],
+}
+
+export interface Row {
+    data: any,
+}
+
 const _props: SheetsProps = {
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
     private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
